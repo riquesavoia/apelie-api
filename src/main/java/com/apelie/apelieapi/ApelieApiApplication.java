c77a05cb20a112fd4ffd8cd3fbe3882b439b843a
Henrique Savoia <henriquestefani@hotmail.com>
2021-04-30 21:16:19 -0300

    Initial commit

---
+++ src/main/java/com/apelie/apelieapi/ApelieApiApplication.java
@@ -0,0 +1,13 @@
+package com.apelie.apelieapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApelieApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApelieApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apelie/apelieapi/ApelieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apelie.apelieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApelieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
