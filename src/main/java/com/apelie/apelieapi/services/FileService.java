16f1ba5b5a31625934b37a8e80597d1c7102c93a
Henrique Savoia <henriquestefani@hotmail.com>
2021-09-20 19:52:05 -0300

    File service documentation

--- src/main/java/com/apelie/apelieapi/services/FileService.java
+++ src/main/java/com/apelie/apelieapi/services/FileService.java
@@ -3,7 +3,26 @@ package com.apelie.apelieapi.services;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
+    /**
+     * Uploads a file based on its base64 content
+     *
+     * @param base64
+     * @return
+     */
     String uploadFile(String base64);
+
+    /**
+     * Uploads a file based on its MultipartFile content
+     *
+     * @param file
+     * @return
+     */
     String uploadFile(MultipartFile file);
+
+    /**
+     * Deletes an image given its url
+     *
+     * @param url
+     */
     void deleteImageByUrl(String url);
 }
